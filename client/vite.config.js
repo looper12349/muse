673786a1b5105354afd6c32821acee662f59e997
Amritesh Indal <amriteshindal29@gmail.com>
2025-03-01 16:32:48 +0530

    Port Configured

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,14 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
